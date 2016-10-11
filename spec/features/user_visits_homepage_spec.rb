ce6cee2151e002c7fd61f554129fd3b24a8a98d0
Caleb <calebcowen@gmail.com>
2016-10-11 12:54:50 -0600

    Add beginning story and start button closes #5

---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature describe 'user visits homepage' do
+  scenario 'user visits homepage and sees storyline and start button' do
+    # As a user
+    # When I visit the root path
+    visit root_path
+    # I should see the beginning of the story
+    expect(page).to have_content('You are on the way to the lunch room')
+    # and I should see a button that says "Start Game"
+    expect(page).to have_button('Start Game')
+  end
+end
