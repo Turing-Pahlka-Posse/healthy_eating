83e8f54b8123532120ce4eefe602a0381a92a9d7
Caleb <calebcowen@gmail.com>
2016-10-21 16:27:27 -0600

    Add click event to change storyline

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,14 @@
-var story = document.getElementById('story-line');
+document.addEventListener('DOMContentLoaded', function(){
+  var story = document.getElementById('story-line');
+  var storyButton = document.getElementById('continue-button');
+  var clickCount = 0;
 
-var storyButton = document.getElementById('continue-button');
-
-storyButton.addEventListener('click', function(){
-  console.log('clicked');
+  storyButton.addEventListener('click', function(){
+    clickCount += 1;
+    if (clickCount === 1){
+      story.innerHTML = 'Everyone trades bits and pieces from their lunches.';
+    } else {
+      story.innerHTML = 'What kind of trades will you make?';
+    }
+  });
 });
