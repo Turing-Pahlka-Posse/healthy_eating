ea30f34399e54b3f255b1ebf051fdb0bec5f32dc
Kerry <kerry.sheldon@gmail.com>
2016-10-22 09:47:32 -0600

    adds story constructor, chapter constructor, ability to get next line within and beteen chapters

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-  var story = document.getElementById('story-line');
-  var storyButton = document.getElementById('continue-button');
-  var clickCount = 0;
+$(document).ready(function(){
+  var story = new Story()
+  var homeChapter = new Chapter("home", ['You are on the way to the lunch room',
+                                         'Everyone trades bits and pieces from their lunches.',
+                                         'What kind of trades will you make?'])
+  story.addChapter(homeChapter);
 
-  storyButton.addEventListener('click', function(){
-    clickCount += 1;
-    if (clickCount === 1){
-      story.innerHTML = 'Everyone trades bits and pieces from their lunches.';
-    } else {
-      story.innerHTML = 'What kind of trades will you make?';
-    }
-  });
+
+  var displayNextLine = function() {
+    var storyElement = document.getElementById('story-line');
+    storyElement.innerHTML = story.getNextLine();
+  }
+
+  var storyButton = document.getElementById('continue-button');
+  storyButton.addEventListener('click', displayNextLine)
+
 });
