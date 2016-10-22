968bb078c669e85248dce1664628160718aa75b2
Kerry <kerry.sheldon@gmail.com>
2016-10-22 08:24:04 -0600

    adds test for second click on continue

--- spec/features/user_clicks_continue_spec.rb
+++ spec/features/user_clicks_continue_spec.rb
@@ -8,5 +8,15 @@ RSpec.feature describe 'user clicks continue' do
 
     expect(page).to have_content('Everyone trades bits and pieces from their lunches.')
     expect(page).to_not have_content('You are on your way to the lunch room.')
+
+    click_button('Continue')
+
+    expect(page).to_not have_content('Everyone trades bits and pieces from their lunches.')
+    expect(page).to_not have_content('You are on your way to the lunch room.')
+
+    expect(page).to have_content('What kind of trades will you make?')
+
   end
+
+
 end
